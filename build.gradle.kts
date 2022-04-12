408abc982cfb11e9270041036a38e9d7efdaa1ec
CatDevz <codyquinn1122@gmail.com>
2022-04-12 00:06:41 -0500

    Add publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,3 +28,15 @@ tasks.withType<Test>().configureEach {
 
     useJUnitPlatform()
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["kotlin"])
+        }
+    }
+}
